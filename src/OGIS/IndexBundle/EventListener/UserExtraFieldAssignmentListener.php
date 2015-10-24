735ef7e7d4ca86f3bd890bf697dc4a1b2968f9e4
Solovyev-Pavel <solovyev.p.a@gmail.com>
2015-10-24 17:57:51 +0300

    Update UserExtraFieldAssignmentListener.php

--- src/OGIS/IndexBundle/EventListener/UserExtraFieldAssignmentListener.php
+++ src/OGIS/IndexBundle/EventListener/UserExtraFieldAssignmentListener.php
@@ -33,50 +33,49 @@ use OGIS\IndexBundle\Entity\Catalog;
 
 class UserExtraFieldAssignmentListener implements EventSubscriberInterface {
 
-	protected $entityManager;
-	protected $aclProvider;
-	protected $tokenStorage;
+		protected $entityManager;
+		protected $aclProvider;
+		protected $tokenStorage;
 
-	public function __construct($entityManager, $aclProvider, $tokenStorage){
-		$this->entityManager = $entityManager;
-		$this->aclProvider = $aclProvider;
-		$this->tokenStorage = $tokenStorage;
-	}
-
-	public static function getSubscribedEvents(){
-		return array( FOSUserEvents::REGISTRATION_COMPLETED => 'onRegistrationSuccessful' );
-	}
-
-	public function onRegistrationSuccessful(UserEvent $event){
-		$user = $event->getUser();
-
-		// set user display name using username as default value
-		$user->setDisplayName($user->getUsername());
-		// update user role to 'proper' value and add limits to user
-		$user->setRoles(array('ROLE_USER'));
-		$limits = $this->entityManager->getRepository('OGIS\IndexBundle\Entity\Role')->findOneBy(array('role' => 'ROLE_USER'));
-		$user->setLimits($limits);
-
-		// create catalogs for the user
-		$catalog = new Catalog();
-		$catalog->addOwner($user);
-		$catalog->setTitle($user->getDisplayName() . '\'s resources');
-		$this->entityManager->persist($catalog);
-		$this->entityManager->flush();
-
-		// bind catalog to user
-		$user->addCatalog($catalog);
-
-		// ACL
-		$objectIdentity = ObjectIdentity::fromDomainObject($catalog);
-		try{
-			$acl = $this->aclProvider->findAcl($objectIdentity);
-        }catch (\Symfony\Component\Security\Acl\Exception\AclNotFoundException $e) {
-			$acl = $this->aclProvider->createAcl($objectIdentity);
+		public function __construct($entityManager, $aclProvider, $tokenStorage){
+				$this->entityManager = $entityManager;
+				$this->aclProvider = $aclProvider;
+				$this->tokenStorage = $tokenStorage;
 		}
-		$securityIdentity = UserSecurityIdentity::fromAccount($user);
-		$acl->insertObjectAce($securityIdentity, MaskBuilder::MASK_OWNER);
-		$this->aclProvider->updateAcl($acl);
 
-	}
+		public static function getSubscribedEvents(){
+				return array( FOSUserEvents::REGISTRATION_COMPLETED => 'onRegistrationSuccessful' );
+		}
+
+		public function onRegistrationSuccessful(UserEvent $event){
+				$user = $event->getUser();
+
+				// set user display name using username as default value
+				$user->setDisplayName($user->getUsername());
+				// update user role to 'proper' value and add limits to user
+				$user->setRoles(array('ROLE_USER'));
+				$limits = $this->entityManager->getRepository('OGIS\IndexBundle\Entity\Role')->findOneBy(array('role' => 'ROLE_USER'));
+				$user->setLimits($limits);
+
+				// create catalogs for the user
+				$catalog = new Catalog();
+				$catalog->addOwner($user);
+				$catalog->setTitle($user->getDisplayName() . '\'s resources');
+				$this->entityManager->persist($catalog);
+				$this->entityManager->flush();
+
+				// bind catalog to user
+				$user->addCatalog($catalog);
+
+				// ACL
+				$objectIdentity = ObjectIdentity::fromDomainObject($catalog);
+				try{
+						$acl = $this->aclProvider->findAcl($objectIdentity);
+				}catch (\Symfony\Component\Security\Acl\Exception\AclNotFoundException $e) {
+						$acl = $this->aclProvider->createAcl($objectIdentity);
+				}
+				$securityIdentity = UserSecurityIdentity::fromAccount($user);
+				$acl->insertObjectAce($securityIdentity, MaskBuilder::MASK_OWNER);
+			$this->aclProvider->updateAcl($acl);
+			}
 }
