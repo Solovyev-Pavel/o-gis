504be06981e61d1d8eaf1961f941c7f7255df633
Solovyev-Pavel <solovyev.p.a@gmail.com>
2015-10-24 18:04:08 +0300

    Create OGISIndexBundle.php

---
+++ src/OGIS/IndexBundle/OGISIndexBundle.php
@@ -0,0 +1,27 @@
+<?php
+
+/* ************************************************************************** *
+ *   Copyright © 2015       Pavel Solovyev (solovyev.p.a@gmail.com)           *
+ *                          Sergey Sevryukov (sevrukovs@gmail.com)            *
+ *                          Alexander Afonin (acer737@yandex.ru)              *
+ *                                                                            *
+ *   Licensed under the Apache License, Version 2.0 (the "License");          *
+ *   you may not use this file except in compliance with the License.         *
+ *   You may obtain a copy of the License at                                  *
+ *               http://www.apache.org/licenses/LICENSE-2.0                   *
+ *                                                                            *
+ *   Unless required by applicable law or agreed to in writing, software      *
+ *   distributed under the License is distributed on an "AS IS" BASIS,        *
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *   See the License for the specific language governing permissions and      *
+ *   limitations under the License.                                           *
+ * ************************************************************************** */
+
+namespace OGIS\IndexBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OGISIndexBundle extends Bundle
+{
+
+}
